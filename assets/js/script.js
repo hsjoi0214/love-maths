5a6a842e38fc967043ce579c01ccf0cd0716f2fc
prakash joshi <prakash.joshi1402@icloud.com>
2024-03-22 16:20:26 +0000

    Added the remaining operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,9 +77,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else if (operator === "%") {
-        return [operand1 - operand2, "subtract"];
     } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "%") {
         return [operand1 % operand2, "divide"];
     } else
         alert(`Unimplemented operator ${operator}`);
@@ -111,8 +111,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2  ;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
